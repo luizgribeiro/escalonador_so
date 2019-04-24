8979145ccf3e336dc0b3f982c68624a8c6d80b31
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2019-04-23 22:00:14 -0300

    pid generator --working (test case preserved)

--- pid_gen.py
+++ pid_gen.py
@@ -9,13 +9,16 @@ def gen_valid_pid(pids_list, initial_pid, max_proc):
         for proc in range(len(pids_list)):
             pnumber = proc
             #pegando primeiro pid nao utilizado na lista
-            print(f"comparando   proc {(initial_pid+proc)} e  list {pids_list[proc]}")
             if pids_list[proc] != initial_pid + proc:
                 pids_list.insert(proc, (initial_pid+proc))
                 return (pids_list, initial_pid+proc)
        #caso seja inserido no primeiro ou no final
         else:
-            pids_list.append((initial_pid+pnumber))
+            if len(pids_list) == 0:
+                pids_list.append((initial_pid))
+            else:
+                npid = initial_pid + len(pids_list) + 1
+                pids_list.append(npid)
             return (pids_list, initial_pid+pnumber)
 
 
@@ -24,6 +27,9 @@ lt = []
 for i in range(14):
 
     lista , pnumber = gen_valid_pid(lt, 10, 20)
+
+    if i % 3 ==0 and i != 0:
+        del lista[randint(0,len(lista))]
 
     print(lista)
     print(pnumber)
