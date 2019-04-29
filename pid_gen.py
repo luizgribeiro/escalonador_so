947c02c4a5c75b8659fe058dd0a1c24badae74bf
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2019-04-29 17:13:40 +0000

    pid generation based on active processes --done (test case included)

--- pid_gen.py
+++ pid_gen.py
@@ -15,23 +15,34 @@ def gen_valid_pid(pids_list, initial_pid, max_proc):
        #caso seja inserido no primeiro ou no final
         else:
             if len(pids_list) == 0:
+                npid = initial_pid + len(pids_list)
                 pids_list.append((initial_pid))
+                pnumber = npid
             else:
-                npid = initial_pid + len(pids_list) + 1
+                npid = initial_pid + len(pids_list)
                 pids_list.append(npid)
-            return (pids_list, initial_pid+pnumber)
+                pnumber = npid
+            return (pids_list, pnumber)
 
 
+"""
+#testcase
 lt = []
 
+#generating 14 pids
 for i in range(14):
 
-    lista , pnumber = gen_valid_pid(lt, 10, 20)
-    print(lista)
+    #generating pid and updating pids_list
+    lt , pnumber = gen_valid_pid(lt, 10, 20)
+    #when more than 7 process exist 
+    if i > 7:
+        #coin tossing to delete process or not
+        value = randint(0,1)
+        if value == 1:
+            #selecting element to be deleted
+            index = randint(0, len(lt) - 1)
+            del lt[index]
+
+    print(lt)
     print(pnumber)
-
-        
-    
-    
-
-    
+"""
